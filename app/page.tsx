411b411bb9c31b8cd551da84b27d25758d00189c
mohsensami <mohsensami88@gmail.com>
2024-02-20 18:25:53 +0330

    signin page added

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <div className="text-sky-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum aut blanditiis inventore cum eligendi
-            voluptatem quibusdam eaque expedita, reprehenderit placeat, mollitia libero! Totam voluptates, inventore a
-            ad asperiores voluptatum praesentium!
-        </div>
-    );
-}
