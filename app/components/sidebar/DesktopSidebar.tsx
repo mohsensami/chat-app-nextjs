1600a7d950a515a1368190139ce21db959c51238
mohsensami <mohsensami88@gmail.com>
2024-02-22 16:28:27 +0330

    Avatar in sidebar desktop

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -3,10 +3,17 @@
 import useRoutes from '@/app/hooks/useRoutes';
 import { useState } from 'react';
 import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
+import Avatar from '../Avatar';
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+    currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
+    console.log(currentUser);
     return (
         <div
             className="
@@ -42,7 +49,7 @@ const DesktopSidebar = () => {
             </nav>
             <nav className="mt-4 flex flex-col justify-between items-center">
                 <div onClick={() => setIsOpen(true)} className="cursor-pointer hover:opacity-75 transition">
-                    Avatar
+                    <Avatar user={currentUser} />
                 </div>
             </nav>
         </div>
