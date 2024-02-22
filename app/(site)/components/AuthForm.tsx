61173c809215c1fb0c848e820c7927c75c93f144
mohsensami <mohsensami88@gmail.com>
2024-02-22 15:07:49 +0330

    implmenting login and redirect and logput and middleware

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,19 +2,27 @@
 
 import Button from '@/app/components/Button';
 import Input from '@/app/components/input/Input';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        if (session?.status === 'authenticated') {
+            router.push('/users');
+        }
+    }, [session?.status, router]);
     const toggleVariant = useCallback(() => {
         if (variant === 'LOGIN') {
             setVariant('REGISTER');
@@ -39,19 +47,21 @@ const AuthForm = () => {
         if (variant === 'REGISTER') {
             axios
                 .post('/api/register', data)
-                //   .then(() => signIn('credentials', {
-                //     ...data,
-                //     redirect: false,
-                //   }))
-                //   .then((callback) => {
-                //     if (callback?.error) {
-                //       toast.error('Invalid credentials!');
-                //     }
+                .then(() =>
+                    signIn('credentials', {
+                        ...data,
+                        redirect: false,
+                    })
+                )
+                .then((callback) => {
+                    if (callback?.error) {
+                        toast.error('Invalid credentials!');
+                    }
 
-                //     if (callback?.ok) {
-                //       router.push('/conversations')
-                //     }
-                //   })
+                    if (callback?.ok) {
+                        router.push('/users');
+                    }
+                })
                 .catch(() => toast.error('Something went wrong!'))
                 .finally(() => setIsLoading(false));
         }
@@ -68,6 +78,7 @@ const AuthForm = () => {
 
                     if (callback?.ok) {
                         toast.success('Logged in!');
+                        router.push('/users');
                     }
                 })
                 .finally(() => setIsLoading(false));
---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { signOut } from 'next-auth/react';
+
+const Users = () => {
+    return (
+        <div className="hidden lg:block lg:pl-80 h-full">
+            <button onClick={() => signOut()}>Logout</button>
+        </div>
+    );
+};
+
+export default Users;
